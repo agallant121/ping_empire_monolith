8f1364d6e89b0ea8f75f25b97c47eb175d2b53eb
Alex Gallant <agallant1201@gmail.com>
2025-07-22 16:20:45 -0400

    Add perform functionality to pipng_website_job to ping a website and create a response

--- app/jobs/ping_website_job.rb
+++ app/jobs/ping_website_job.rb
@@ -1,7 +1,34 @@
+require "net/http"
+require "uri"
+
 class PingWebsiteJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(website_id)
+    website = Website.find(params[:website_id])
+    return unless website
+
+    uri = URI.parse(website.url)
+    start_time= Time.now
+
+    begin
+      response = Net::HTTP.get_response(uri)
+      end_time = Time.now
+      status_code = response.code.to_i
+      error = nil
+    rescue => e
+      status_code = nil
+      end_time = Time.now
+      error = e.message
+    end
+
+    response_time = ((end_time - start_time) * 1000).to_i
+
+    website.responses.create!(
+      status_code: status_code
+      response_time: response_time,
+      error: error  
+    )
+
   end
 end
