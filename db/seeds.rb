5a4d99e3dd049c1a056b64f133d69c6ec3fd62a2
Alex Gallant <agallant1201@gmail.com>
2025-07-22 16:12:19 -0400

    Add basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Website.destroy_all
+
+websites = Website.create!([
+  { url: "https://google.com" },
+  { url: "https://yahoo.com" },
+  { url: "https://askjeeves" }
+])
+
+websites.each do |website|
+  3.times do
+    website.responses.create!(
+      status_code: [200, 404, 500].sample,
+      response_time: rand(100..1000),
+      checked_at: Time.current - rand(1..12).hours,
+      error: [nil, "Timeout", "Connection refused"].sample
+    )
+  end
+end
+
+puts "Seeded #{Website.count} websites and #{Response.count} responses."
