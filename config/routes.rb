d0f3a39d75e88fa74fda0c1587bdc0db9bc29c02
Alex Gallant <agallant1201@gmail.com>
2025-07-22 16:08:19 -0400

    Update root path to be websites index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "websites#index"
+  
   resources :responses
   resources :websites
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
