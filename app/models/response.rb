912a7dc8867ef50746ef9c6c0b56591a3e5c570f
Alex Gallant <agallant1201@gmail.com>
2025-07-29 11:36:41 -0400

    Add scope and validations to response

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,8 @@
 class Response < ApplicationRecord
   belongs_to :website
+
+  validates :status_code, numericality: true, allow_nil: true
+  validates :response_time, numericality: true, allow_nil: true
+
+  scope :more_than_one_day_old, -> { where("created_at < ?", 1.day.ago) }
 end
