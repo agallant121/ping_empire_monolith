8af4f99f653ca00f566adba9fe862c29004edd6f
Alex Gallant <agallant1201@gmail.com>
2025-07-31 16:05:09 -0400

    Debugging

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,19 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  include Warden::Test::Helpers
 
   setup do
-    @user = users(:test_user)
-    sign_in @user
+    Warden.test_mode!
+  end
+
+  teardown do
+    Warden.test_reset!
+  end
+
+  def sign_in(user)
+    login_as(user, scope: :user)
   end
 end
