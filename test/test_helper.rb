72481a90abdfb5532092ba2e88d7ac7961269737
Alex Gallant <agallant1201@gmail.com>
2025-07-31 15:25:27 -0400

    WIP debugging for ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,8 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+# Enable Devise helpers in integration (request) tests
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+end
