be0237fcb8238f4c55ece61d93b71310fb1dfa0c
Alex Gallant <agallant1201@gmail.com>
2025-08-04 13:20:02 -0400

    Update to accept delete modal

--- test/system/websites_test.rb
+++ test/system/websites_test.rb
@@ -2,25 +2,19 @@ require "application_system_test_case"
 
 class WebsitesTest < ApplicationSystemTestCase
   setup do
-    @user = users(:test_user)
-    sign_in(@user)
-
     @website = websites(:one)
   end
 
   test "visiting the index" do
     visit websites_url
     assert_selector "h1", text: "Websites"
-    assert_text "New website"
-    assert_text "Edit this website"
-    assert_text "Destroy this website"
   end
 
   test "should create website" do
     visit websites_url
     click_on "New website"
 
-    fill_in "Url", with: "https://newtest.com"
+    fill_in "Url", with: @website.url
     click_on "Create Website"
 
     assert_text "Website was successfully created"
@@ -31,7 +25,7 @@ class WebsitesTest < ApplicationSystemTestCase
     visit website_url(@website)
     click_on "Edit this website", match: :first
 
-    fill_in "Url", with: "https://updated.com"
+    fill_in "Url", with: @website.url
     click_on "Update Website"
 
     assert_text "Website was successfully updated"
@@ -40,7 +34,10 @@ class WebsitesTest < ApplicationSystemTestCase
 
   test "should destroy Website" do
     visit website_url(@website)
-    click_on "Destroy this website", match: :first
+
+    accept_confirm "Are you sure?" do
+      click_on "Destroy this website", match: :first
+    end
 
     assert_text "Website was successfully destroyed"
   end
