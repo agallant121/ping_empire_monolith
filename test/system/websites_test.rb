bc2b379ca6d86ad28665667bc602006a1e3ddb49
Alex Gallant <agallant1201@gmail.com>
2025-08-04 12:49:28 -0400

    Add sign in to websites test

--- test/system/websites_test.rb
+++ test/system/websites_test.rb
@@ -2,19 +2,25 @@ require "application_system_test_case"
 
 class WebsitesTest < ApplicationSystemTestCase
   setup do
+    @user = users(:test_user)
+    sign_in(@user)
+
     @website = websites(:one)
   end
 
   test "visiting the index" do
     visit websites_url
     assert_selector "h1", text: "Websites"
+    assert_text "New website"
+    assert_text "Edit this website"
+    assert_text "Destroy this website"
   end
 
   test "should create website" do
     visit websites_url
     click_on "New website"
 
-    fill_in "Url", with: @website.url
+    fill_in "Url", with: "https://newtest.com"
     click_on "Create Website"
 
     assert_text "Website was successfully created"
@@ -25,7 +31,7 @@ class WebsitesTest < ApplicationSystemTestCase
     visit website_url(@website)
     click_on "Edit this website", match: :first
 
-    fill_in "Url", with: @website.url
+    fill_in "Url", with: "https://updated.com"
     click_on "Update Website"
 
     assert_text "Website was successfully updated"
