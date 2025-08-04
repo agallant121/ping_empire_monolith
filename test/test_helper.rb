6a20dd8d080c4507b73819f41840ce306ce14562
Alex Gallant <agallant1201@gmail.com>
2025-08-04 11:06:55 -0400

    WIP debugging help

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,13 +4,8 @@ require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
 
@@ -18,3 +13,21 @@ end
 class ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 end
+
+# ✅ Capybara system test setup with unique Chrome user data directory
+require "capybara/rails"
+require "selenium/webdriver"
+require "securerandom"
+
+Capybara.register_driver :chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  unique_dir = "/tmp/chrome-user-data-#{SecureRandom.hex(8)}"
+  options.add_argument("--user-data-dir=#{unique_dir}")
+  options.add_argument("--headless") # remove this line if you want GUI
+  options.add_argument("--disable-gpu")
+  options.add_argument("--no-sandbox")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome
