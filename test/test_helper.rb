16e0909836da1a9f1709dc80e0d0d88cbd42be2f
Alex Gallant <agallant1201@gmail.com>
2025-08-04 11:13:52 -0400

    WIP debugging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,20 +4,16 @@ require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
   end
 end
 
-# Enable Devise helpers in integration (request) tests
 class ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 end
 
-# ✅ Capybara system test setup with unique Chrome user data directory
 require "capybara/rails"
 require "selenium/webdriver"
 require "tmpdir"
@@ -25,7 +21,6 @@ require "tmpdir"
 Capybara.register_driver :selenium_chrome_headless_unique do |app|
   options = Selenium::WebDriver::Chrome::Options.new
 
-  # ✅ Unique temp directory to prevent CI conflicts
   options.add_argument("--user-data-dir=#{Dir.mktmpdir("chrome-user-data")}")
   options.add_argument("--headless=new") # Use new headless mode if supported
   options.add_argument("--disable-gpu")
@@ -35,5 +30,4 @@ Capybara.register_driver :selenium_chrome_headless_unique do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-# ✅ Set as the default JavaScript driver
 Capybara.javascript_driver = :selenium_chrome_headless_unique
