f895e46aef03cd3ce1244c13133ff2f4b2362d20
Alex Gallant <agallant1201@gmail.com>
2025-09-09 16:02:02 -0400

    Add uniqueness and url validations for creating a website for a user

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,7 +1,11 @@
+require "uri"
+
 class Website < ApplicationRecord
   belongs_to :user
 
   has_many :responses, dependent: :destroy
 
-  validates :url, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true,
+                  format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "must start with http:// or https://" },
+                  uniqueness: { scope: :user_id, message: "has already been added for the current user" }
 end
