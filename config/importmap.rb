f7ab684fe4a1a1df53afcb0fd5ea37dd8644a794
Alex Gallant <agallant1201@gmail.com>
2025-09-10 15:29:18 -0400

    Update show view for websites

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,4 +9,3 @@ pin "bootstrap", integrity: "sha384-RrYZu0SIUSWw6KnQ9e4NU0mfuqJa4y/zsRQiX/eApHrP
 pin "@popperjs/core", to: "@popperjs--core.js", integrity: "sha384-bfekMOfeUlr1dHZfNaAFiuuOeD7r+Qh45AQ2HHJY7EAAI4QGJ6qx1Qq9gsbvS+60" # @2.11.8
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
 pin "bootstrap.min.css", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-
