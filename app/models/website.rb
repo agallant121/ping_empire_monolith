1fd6c63ae15630e78346799d025e7f89112d51c2
Alex Gallant <agallant1201@gmail.com>
2025-09-12 15:39:09 -0400

    Add latest_response method on website

--- app/models/website.rb
+++ app/models/website.rb
@@ -8,4 +8,8 @@ class Website < ApplicationRecord
   validates :url, presence: true, uniqueness: true,
                   format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "must start with http:// or https://" },
                   uniqueness: { scope: :user_id, message: "has already been added for the current user" }
+
+  def latest_response
+    @latest_response ||= responses.order(created_at: :desc).first
+  end
 end
