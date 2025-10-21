1ca763525716b850fa41390869958b3889c316ab
Alex Gallant <agallant1201@gmail.com>
2025-10-21 13:04:29 -0400

    Remove dashboards and use websites controller

--- app/controllers/dashboards_controller.rb
+++
@@ -1,9 +0,0 @@
-class DashboardsController < ApplicationController
-  before_action :authenticate_user!
-
-  def show
-    @websites = current_user.websites
-    @websites = @websites.where("url ILIKE ?", "%#{params[:q]}%") if params[:q].present?
-    @websites = @websites.recent.includes(:responses).page(params[:page]).per(12)
-  end
-end
