7d936459f57f2a48b4f43d915bf672ac801d50be
Alex Gallant <agallant1201@gmail.com>
2025-10-22 15:20:52 -0400

    Update to only clear cache for specific users so a website being changed globally will not clear every users cache

--- app/jobs/ping_all_websites_job.rb
+++ app/jobs/ping_all_websites_job.rb
@@ -2,22 +2,25 @@ class PingAllWebsitesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    User.find_each do |user|
+      cache_key = "website_ids_user_#{user.id}"
 
-    ids = Rails.cache.read("website_ids") || []
-    current_ids = Website.pluck(:id)
+      ids = Rails.cache.read(cache_key) || []
+      current_ids = user.websites.pluck(:id)
 
-    current_ids_and_cached_ids_different = ids.sort != current_ids.sort
+      current_ids_and_cached_ids_different = ids.sort != current_ids.sort
 
-    if current_ids_and_cached_ids_different
-      Rails.cache.write("website_ids", current_ids, expires_in: 12.hours)
-      Rails.logger.info "Website list changed, updating cache"
-      ids = current_ids
-    else
-      Rails.logger.info "Website list unchanged, using cached data"
-    end
+      if current_ids_and_cached_ids_different
+        Rails.cache.write(cache_key, current_ids, expires_in: 12.hours)
+        Rails.logger.info "Website list changed for user #{user.id}, updating cache"
+        ids = current_ids
+      else
+        Rails.logger.info "Website list unchanged for user #{user.id}, using cached data"
+      end
 
-    ids.each do |website_id|
-      PingWebsiteJob.perform_later(website_id)
+      ids.each do |website_id|
+        PingWebsiteJob.perform_later(website_id)
+      end
     end
   end
 end
