147a102dba6079b64fba95487f316c3193db17ad
Alex Gallant <agallant1201@gmail.com>
2025-10-22 14:46:15 -0400

    Cache website ids to send pings instead of full wesbite db lookup every five minutes

--- app/jobs/ping_all_websites_job.rb
+++ app/jobs/ping_all_websites_job.rb
@@ -2,8 +2,16 @@ class PingAllWebsitesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Website.find_each do |website|
-      PingWebsiteJob.perform_later(website.id)
+    website_ids = Rails.cache.fetch("website_ids", expires_in: 12.hours) do
+      ids = Website.pluck(:id)
+      Rails.logger.info "Caching website list: #{ids.size} websites"
+      ids
+    end
+
+    Rails.logger.info "Using cached website list (#{website_ids.size} websites)"
+
+    website_ids.each do |website_id|
+      PingWebsiteJob.perform_later(website_id)
     end
   end
 end
