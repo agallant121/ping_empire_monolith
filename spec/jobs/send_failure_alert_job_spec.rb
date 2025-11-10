bd4506264bceca66177d23a887c9f5cdd9fe4b83
Alex Gallant <agallant1201@gmail.com>
2025-11-10 11:20:51 -0500

    Add job specs for failure alerts

--- spec/jobs/send_failure_alert_job_spec.rb
+++ spec/jobs/send_failure_alert_job_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe SendFailureAlertJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(email: "user@example.com", password: "password") }
+  let(:website) { user.websites.create!(url: "https://example.com") }
+  let(:response) { website.responses.create!(status_code: 500, response_time: 100) }
+  let(:mailer_double) { instance_double(ActionMailer::MessageDelivery, deliver_now: true) }
+
+  before do
+    allow(NotificationMailer).to receive(:failure_alert).and_return(mailer_double)
+  end
+
+  it "delivers a failure alert when a user is present" do
+    described_class.perform_now(website, response)
+
+    expect(NotificationMailer).to have_received(:failure_alert).with(user, website, response)
+    expect(mailer_double).to have_received(:deliver_now)
+  end
+
+  it "does not deliver a failure alert when the website has no user" do
+    described_class.perform_now(double("Website", user: nil), response)
+
+    expect(NotificationMailer).not_to have_received(:failure_alert)
+  end
 end
