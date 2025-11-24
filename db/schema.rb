5e3c4999316a313a0b99eae2a17ef6fa1b5ae8fd
Alex Gallant <agallant1201@gmail.com>
2025-11-24 09:01:17 -0500

    Add migration to add preferred language to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_10_120000) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_10_130000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_10_120000) do
     t.string "provider"
     t.string "uid"
     t.integer "role", default: 0, null: false
+    t.string "preferred_language", default: "en", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
